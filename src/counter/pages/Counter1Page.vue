3394569c5e4e9bfa7b694b3dfb0bda8909ba1316
ctola91 <ctolapacheco@gmail.com>
2023-03-15 21:07:45 -0400

    add pinia store

--- src/counter/pages/Counter1Page.vue
+++ src/counter/pages/Counter1Page.vue
@@ -1,9 +1,15 @@
 <script setup lang="ts">
-import { defineComponent } from 'vue';
+import { useCounterOptionsStore } from "@/store/counter-options";
+import { storeToRefs } from "pinia";
+import { defineComponent } from "vue";
 
+const counterOptionsStore = useCounterOptionsStore();
+const { count, squareCount } = storeToRefs(counterOptionsStore);
 </script>
 <template>
-  <div></div>
+  <h1>Pinia Options</h1>
+  <h4>Counter: {{ counterOptionsStore.count }}</h4>
+  <h4>Square: {{ counterOptionsStore.squareCount }}</h4>
 </template>
 
 <style scoped></style>
--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,16 @@
-import { createApp } from "vue"
-import { createPinia } from "pinia"
+import { createApp } from "vue";
+import { createPinia } from "pinia";
 
-import App from "./App.vue"
-import router from "./router"
+import App from "./App.vue";
+import router from "./router";
 
-const pinia = createPinia()
+import "./assets/main.css";
 
-import "./assets/main.css"
+const pinia = createPinia();
 
-const app = createApp(App)
+const app = createApp(App);
 
-app.use(pinia)
-app.use(router)
+app.use(pinia);
+app.use(router);
 
-app.mount("#app")
+app.mount("#app");
