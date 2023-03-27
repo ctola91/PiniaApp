96a46302223acb1df7c275b022b961e10e93d445
ctola91 <ctolapacheco@gmail.com>
2023-03-26 21:56:43 -0400

    add new component vor client list

--- src/clients/pages/ListPage.vue
+++ src/clients/pages/ListPage.vue
@@ -1,7 +1,14 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import ClientList from "@/clients/components/ClientList.vue";
+</script>
 <template>
   <div>
     <h1>Clients List</h1>
+    <!-- Client List -->
+    <ClientList />
+    <!-- PaginationNumbers  -->
+
+    <!-- LoadingModal -->
   </div>
 </template>
 
