a569514831ba9b0d99637177b73b9c1dde43c7f2
ctola91 <ctolapacheco@gmail.com>
2023-03-28 22:28:14 -0400

    logic of pagination

--- src/clients/composables/useClients.ts
+++ src/clients/composables/useClients.ts
@@ -3,7 +3,7 @@ import clientsApi from "@/api/clients-api";
 import type { Client } from "@/clients/interfaces/client";
 import { useClientsStore } from "@/store/clients";
 import { storeToRefs } from "pinia";
-import { watch } from "vue";
+import { computed, watch } from "vue";
 
 const getClients = async (): Promise<Client[]> => {
   const { data } = await clientsApi.get<Client[]>("/clients?_page=1");
@@ -25,6 +25,14 @@ const useClients = () => {
     currentPage,
     isLoading,
     totalPages,
+
+    // Getters
+    totalPageNumbers: computed(() => [...new Array(totalPages.value)].map((v, i) => i + 1)),
+    // methods
+    // getPage(page: number) {
+    //   store.setPage(page);
+    // }
+    getPage: store.setPage
   };
 };
 
